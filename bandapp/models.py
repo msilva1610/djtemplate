b203e1301e176b2202a840a7cbb873c8535701e3
Maurilio Silva <Mau@MacBook-Air-de-Maurilio.local>
2017-12-30 19:40:29 -0200

    add bandapp aplication

---
+++ bandapp/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+# Create your models here.
+class Band(models.Model):
+
+    """A model of a rock band."""
+    name = models.CharField(max_length=200)
+    can_rock = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'band'
+        verbose_name_plural = 'bands'
+
+    def __str__(self):
+        return self.name
+
+class Member(models.Model):
+
+    """A model of a rock band member."""
+    name = models.CharField("Member's name", max_length=200)
+    instrument = models.CharField(choices=(
+        ('g', "Guitar"),
+        ('b', "Bass"),
+        ('d', "Drums"),
+        ('v', "Vocal"),
+        ('p', "Piano"),
+    ),
+        max_length=1
+    )
+
+    band = models.ForeignKey("Band", related_name='band')
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'member'
+        verbose_name_plural = 'members'
+
+    def __str__(self):
+        return self.name
--- mysite/settings.py
+++ mysite/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # My APPs
     'core',
+    'bandapp',
     # pip install django-bootstrap3
     'bootstrap3',
+    # pip install django-tables2
+    'django_tables2',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +68,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # django-tables2
+                'django.template.context_processors.request',
             ],
         },
     },
